17492b97ae3de1410dd0ddfcb62ff8861ebdedca
julian1vidal <63807211+julian1vidal@users.noreply.github.com>
2023-05-23 14:11:34 -0300

    optimizado un poco eigen.cpp

--- src/eigen.cpp
+++ src/eigen.cpp
@@ -5,23 +5,23 @@ using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-pair<double, VectorXd> powerIteration(MatrixXd, int, double);
+pair<double, VectorXd> powerIteration(MatrixXd, int, double, int n);
 pair<VectorXd, MatrixXd> eigen(MatrixXd, int, double);
-VectorXd normalize(VectorXd v);
-MatrixXd agregarVecAColumna(MatrixXd A, VectorXd v, int col);
+VectorXd normalize(VectorXd v, int n);
+MatrixXd agregarVecAColumna(MatrixXd A, VectorXd v, int col, int n);
 
-pair<double, VectorXd> powerIteration(MatrixXd A, int niter, double eps)
+pair<double, VectorXd> powerIteration(MatrixXd A, int niter, double eps, int n)
 {
-    VectorXd b = normalize(VectorXd::Random(A.cols()));
-    VectorXd old(A.cols());
-    VectorXd new_b(A.cols());
+    VectorXd b = normalize(VectorXd::Random(n), n);
+    VectorXd old(n);
+    VectorXd new_b(n);
     bool stop = false;
 
     int i = 0;
     while (i < niter && !stop)
     {
         old = b;
-        new_b = normalize(A * b);
+        new_b = normalize(A * b, n);
 
         double cosAngle = new_b.dot(old);
         if (((1 - eps) < cosAngle) && cosAngle <= 1)
@@ -38,15 +38,16 @@ pair<double, VectorXd> powerIteration(MatrixXd A, int niter, double eps)
 
 pair<VectorXd, MatrixXd> eigen(MatrixXd A, int niter, double eps)
 {
-    VectorXd eigenvalues(A.cols());
-    MatrixXd AV(A.cols(), A.cols());
+    int n = A.cols();
+    VectorXd eigenvalues(n);
+    MatrixXd AV(n, n);
     pair<double, VectorXd> temp;
 
-    for (int i = 0; i < A.cols(); i++)
+    for (int i = 0; i < n; i++)
     {
-        temp = powerIteration(A, niter, eps);
+        temp = powerIteration(A, niter, eps, n);
         eigenvalues[i] = temp.first;
-        AV = agregarVecAColumna(AV, temp.second, i);
+        AV = agregarVecAColumna(AV, temp.second, i, n);
         A -= temp.first * (temp.second * temp.second.transpose());
         cout << i << endl;
     }
@@ -54,26 +55,26 @@ pair<VectorXd, MatrixXd> eigen(MatrixXd A, int niter, double eps)
     return res;
 }
 
-VectorXd normalize(VectorXd v)
+VectorXd normalize(VectorXd v, int n)
 {
     double sum = 0.00;
-    for (int i = 0; i < v.size(); i++)
+    for (int i = 0; i < n; i++)
     {
         sum += double(v[i] * v[i]);
     }
     sum = double(sqrt(sum));
 
-    for (int j = 0; j < v.size(); j++)
+    for (int j = 0; j < n; j++)
     {
         v[j] /= sum;
     }
     return v;
 }
 
-MatrixXd agregarVecAColumna(MatrixXd A, VectorXd v, int col)
+MatrixXd agregarVecAColumna(MatrixXd A, VectorXd v, int col, int n)
 {
     // Agrega el vector v a la columna i de la matriz A
-    for (int i = 0; i < A.cols(); i++)
+    for (int i = 0; i < n; i++)
     {
         A(i, col) = v[i];
     }
