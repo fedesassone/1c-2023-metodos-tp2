f27a1148db18cdc4546a2e1e503ad37e321a254b
julian1vidal <63807211+julian1vidal@users.noreply.github.com>
2023-05-23 15:18:41 -0300

    eigen optimizado

--- src/eigen.cpp
+++ src/eigen.cpp
@@ -10,32 +10,6 @@ pair<VectorXd, MatrixXd> eigen(MatrixXd, int, double);
 VectorXd normalize(VectorXd v, int n);
 MatrixXd agregarVecAColumna(MatrixXd A, VectorXd v, int col, int n);
 
-pair<double, VectorXd> powerIteration(MatrixXd A, int niter, double eps, int n)
-{
-    VectorXd b = normalize(VectorXd::Random(n), n);
-    VectorXd old(n);
-    VectorXd new_b(n);
-    bool stop = false;
-
-    int i = 0;
-    while (i < niter && !stop)
-    {
-        old = b;
-        new_b = normalize(A * b, n);
-
-        double cosAngle = new_b.dot(old);
-        if (((1 - eps) < cosAngle) && cosAngle <= 1)
-        {
-            stop = true;
-        }
-        b = new_b;
-        i++;
-    }
-    new_b = A * b;
-    double eigenvalue = b.dot(new_b);
-    return make_pair(eigenvalue, b);
-}
-
 pair<VectorXd, MatrixXd> eigen(MatrixXd A, int niter, double eps)
 {
     int n = A.cols();
@@ -45,31 +19,65 @@ pair<VectorXd, MatrixXd> eigen(MatrixXd A, int niter, double eps)
 
     for (int i = 0; i < n; i++)
     {
+        cout << "autovalor: " << i << endl;
+        cout << "arranca powerIteration" << endl;
+
         temp = powerIteration(A, niter, eps, n);
+
+        cout << "termino powerIteration " << endl;
+
         eigenvalues[i] = temp.first;
         AV = agregarVecAColumna(AV, temp.second, i, n);
         A -= temp.first * (temp.second * temp.second.transpose());
-        cout << i << endl;
+
+        cout << "termino autovalor " << i << endl;
+        cout << "======================" << endl;
+
     }
     pair<VectorXd, MatrixXd> res = make_pair(eigenvalues, AV);
     return res;
 }
 
-VectorXd normalize(VectorXd v, int n)
+pair<double, VectorXd> powerIteration(MatrixXd A, int niter, double eps, int n)
 {
-    double sum = 0.00;
-    for (int i = 0; i < n; i++)
-    {
-        sum += double(v[i] * v[i]);
-    }
-    sum = double(sqrt(sum));
+    VectorXd b = VectorXd::Random(n);
 
-    for (int j = 0; j < n; j++)
+    VectorXd old(n);
+    bool stop = false;
+
+    int i = 0;
+    while (i < niter && !stop)
     {
-        v[j] /= sum;
+        old = b;
+        b = (A * b).normalized();
+
+        double cosAngle = b.dot(old);
+        if (((1 - eps) < cosAngle) && cosAngle <= 1)
+        {
+            stop = true;
+        }
+        i++;
     }
-    return v;
+    double eigenvalue = b.dot(A * b);
+    return make_pair(eigenvalue, b);
 }
+
+
+// VectorXd normalize(VectorXd v, int n)
+// {
+//     double sum = 0.00;
+//     for (int i = 0; i < n; i++)
+//     {
+//         sum += double(v[i] * v[i]);
+//     }
+//     sum = double(sqrt(sum));
+
+//     for (int j = 0; j < n; j++)
+//     {
+//         v[j] /= sum;
+//     }
+//     return v;
+// }
 
 MatrixXd agregarVecAColumna(MatrixXd A, VectorXd v, int col, int n)
 {
